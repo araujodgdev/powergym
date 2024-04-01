2ad966219d24c46da5975302d4e1382d7376ca4f
Douglas Araújo <dcpa@cesar.school>
2024-04-01 02:08:13 -0300

    feat: Cria a página de cadastro

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import { vastShadow } from "@/fonts";
+import SignupForm from "@/components/signup-form";
+
+export default function SignUp() {
+    return (
+        <main className="flex h-full items-center justify-center bg-zinc-900">
+        <section className="flex p-20 w-full flex-col items-center justify-center">
+            <div className="flex flex-col items-center justify-center">
+            <h1 className={`font-bold text-4xl text-orange-500 drop-shadow-sm ${vastShadow.className}`}>POWER<span className="text-amber-100">GYM</span></h1>
+            </div>
+            <SignupForm />
+        </section>
+        <section className="hidden md:flex md:items-center md:justify-center">
+            <Image src='/login-icon.png' alt="Login Icon" width={1600} height={2400} priority className="w-[2000px]"/>
+        </section>
+        </main>
+    );
+}
--- src/components/signup-form.tsx
+++ src/components/signup-form.tsx
@@ -16,36 +16,57 @@ import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import { useRouter } from "next/navigation";
 
-const loginFormSchema = z.object({
+const signupFormSchema: any = z.object({
+  name: z.string().min(3, {
+    message: "O nome deve ter no mínimo 3 caracteres",
+  }),
   email: z.string().email({
     message: 'O e-mail deve ser um endereço de e-mail válido'
   }),
   password: z.string().min(8, {
     message: "A senha deve ter no mínimo 8 caracteres",
   }),
+  confirmPassword: z.string().min(8).refine((value) => value === signupFormSchema.password, {
+    message: "As senhas devem ser iguais",
+  })
 });
 
-export default function LoginForm() {
+export default function SignupForm() {
   const router = useRouter();
-
-  const form = useForm<z.infer<typeof loginFormSchema>>({
-    resolver: zodResolver(loginFormSchema),
+  const form = useForm<z.infer<typeof signupFormSchema>>({
+    resolver: zodResolver(signupFormSchema),
     defaultValues: {
       email: "",
       password: "",
     },
   });
 
-  function onSubmit(values: z.infer<typeof loginFormSchema>) {
-    const { email, password } = values;
-
-    console.log(`Email: ${email}`);
-    console.log(`Password: ${password}`);
+  function onSubmit(values: z.infer<typeof signupFormSchema>) {
+    console.log(values)
   }
 
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="w-full p-6">
+      <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem className="mb-8">
+              <FormLabel>Seu nome:</FormLabel>
+              <FormControl>
+                <Input
+                  type="text"
+                  placeholder="seu nome"
+                  className="text-neutral-900"
+                  {...field}
+                />
+              </FormControl>
+              <FormDescription></FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
         <FormField
           control={form.control}
           name="email"
@@ -84,13 +105,32 @@ export default function LoginForm() {
             </FormItem>
           )}
         />
+        <FormField
+          control={form.control}
+          name="confirmPassword"
+          render={({ field }) => (
+            <FormItem className="mb-8">
+              <FormLabel>Confirme sua senha:</FormLabel>
+              <FormControl>
+                <Input
+                  type="password"
+                  placeholder="********"
+                  className="text-neutral-900"
+                  {...field}
+                />
+              </FormControl>
+              <FormDescription></FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
         <div className="flex flex-col gap-4">
           <Button
             type="submit"
             variant="default"
             className="hover:bg-orange-600  w-full text-lg uppercase font-bold"
           >
-            Entrar
+            Cadastrar
           </Button>
           <div className="flex items-center justify-center gap-4">
             <div className="h-[1.5px] bg-white w-full"></div>
@@ -101,9 +141,9 @@ export default function LoginForm() {
             type="button"
             variant="secondary"
             className="hover:bg-orange-800 bg-transparent border-2 border-orange-600 text-white font-bold text-lg uppercase"
-            onClick={() => router.push("/signup")}
+            onClick={() => router.push("/login")}
           >
-            Cadastrar
+            Entrar
           </Button>
         </div>
       </form>
