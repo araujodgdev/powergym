90f3e116af7db6fdf727973b0ea20bc5fea139a1
Douglas Araújo <dcpa@cesar.school>
2024-04-01 01:34:18 -0300

    style: adiciona uma imagem na página de login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,15 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex h-full">
-      <section className="flex flex-col items-center justify-center bg-zinc-950 flex-grow-[0.1]">
+    <main className="flex h-full items-center justify-center bg-zinc-900">
+      <section className="flex p-20 w-full flex-col items-center justify-center">
         <div className="flex flex-col items-center justify-center">
           <h1 className={`font-bold text-4xl text-orange-500 drop-shadow-sm ${vastShadow.className}`}>POWER<span className="text-amber-100">GYM</span></h1>
         </div>
         <LoginForm />
       </section>
-      <section className="hidden bg-zinc-800 md:flex md:flex-grow">
-        World
+      <section className="hidden md:flex md:items-center md:justify-center">
+        <Image src='/login-icon.png' alt="Login Icon" width={1600} height={2400} className="w-[2000px]"/>
       </section>
     </main>
   );
